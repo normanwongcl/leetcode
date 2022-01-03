cb4b00a2ebfc814bf1e4ccf90919697c30d0d84b
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-03 22:02:50 +0800

    Time: 117 ms (12.38%), Space: 45.2 MB (5.45%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -0,0 +1,25 @@
+/** Using hash Map
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = (n) => {
+    const lookUpAnswer = {3: "Fizz", 5: "Buzz"};
+    const answer = [];
+    
+    for (let index = 1; index <= n; index++) {
+        let answerString = '';
+        
+        for (const [key, value] of Object.entries(lookUpAnswer)) {
+            if (index % key === 0) {
+                answerString += value;
+            }
+        }
+        
+        if (!answerString) {
+            answerString += index;
+        }
+        
+        answer.push(answerString);
+    }
+    return answer;
+};
