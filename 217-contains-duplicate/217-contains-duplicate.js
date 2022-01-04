31ac1417da8919be06b9c9221e5e387b62d4c952
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-04 09:13:07 +0800

    Time: 132 ms (28.41%), Space: 49 MB (12.30%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = (nums) => {
+    const set = {};
+    
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+        if (!set[element]) {
+            set[element] = true;;
+        } else {
+            return true;
+        }
+    }
+    return false;
+};
