697417d2e45ef2ee16736ea2bd10905dbdbda9e8
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-07 13:51:12 +0800

    Time: 259 ms (35.18%), Space: 49.2 MB (62.32%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,32 @@
+/** Using 2-pointers
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = (nums) => {
+    nums.sort((a, b) => a - b);
+	const triplets = [];
+	
+	for (let i =0; i< nums.length - 2; i++) {
+		let left = i + 1;
+		let right = nums.length - 1;
+        if (nums[i-1] !== nums[i] || i === 0) {
+            while (left < right) {
+                const currentSum = nums[i] + nums[left] + nums[right];
+
+                if (currentSum === 0) {
+                    triplets.push([nums[i], nums[left], nums[right]]);
+                    left++;
+                    right--;
+                    while (left < right && nums[left] === nums[left - 1]) {
+                        left++;
+                    }
+                } else if (currentSum < 0) {
+                    left++;
+                } else if (currentSum > 0) {
+                    right--;
+                }
+            }
+        }
+    }
+	return triplets;
+}
