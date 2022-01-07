f265034dbd318fa2c1bf48c10c4eb5e16e182904
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-07 13:56:20 +0800

    Time: 290 ms (49.23%), Space: 50.3 MB (51.06%) - LeetHub

--- 15-3sum/15-3sum.ts
+++ 15-3sum/15-3sum.ts
@@ -2,30 +2,30 @@ type Triplet = [number, number, number];
 
 // Using 2-pointers
 const threeSum = (nums: number[]): number[][] => {
-    nums.sort((a, b) => a - b);
-	const triplets:Triplet[] = [];
-	
-	for (let i =0; i< nums.length - 2; i++) {
-		let left = i + 1;
-		let right = nums.length - 1;
-        if (nums[i-1] !== nums[i] || i === 0) {
-            while (left < right) {
-                const currentSum = nums[i] + nums[left] + nums[right];
+  nums.sort((a, b) => a - b);
+  const triplets: Triplet[] = [];
 
-                if (currentSum === 0) {
-                    triplets.push([nums[i], nums[left], nums[right]]);
-                    left++;
-                    right--;
-                    while (left < right && nums[left] === nums[left - 1]) {
-                        left++;
-                    }
-                } else if (currentSum < 0) {
-                    left++;
-                } else if (currentSum > 0) {
-                    right--;
-                }
-            }
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    if (nums[i - 1] !== nums[i] || i === 0) {
+      while (left < right) {
+        const currentSum = nums[i] + nums[left] + nums[right];
+
+        if (currentSum === 0) {
+          triplets.push([nums[i], nums[left], nums[right]]);
+          left++;
+          right--;
+          while (left < right && nums[left] === nums[left - 1]) {
+            left++;
+          }
+        } else if (currentSum < 0) {
+          left++;
+        } else if (currentSum > 0) {
+          right--;
         }
+      }
     }
-	return triplets;
-}
+  }
+  return triplets;
+};
