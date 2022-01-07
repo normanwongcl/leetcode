98b828c31643e4bcb57c60cdfdae1354fc66e9f8
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-07 15:52:23 +0800

    Time: 198 ms (24.53%), Space: 55.7 MB (24.53%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.ts
@@ -0,0 +1,13 @@
+const findDuplicate = (nums: number[]): number => {
+  const set: Set<number> = new Set();
+
+  for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+    if (!set.has(element)) {
+      set.add(element);
+    } else {
+      return element;
+    }
+  }
+  return -1;
+};
