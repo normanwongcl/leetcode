042683047529599c4ddf97bcfee4a4df989e0ca7
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-14 22:48:24 +0800

    Time: 180 ms (5.18%), Space: 40.5 MB (76.79%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/** 
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = (head) => {
+    let current = head; 
+    let prev = null;
+    
+    while(current) {
+        let newTemp = current.next;
+        current.next = prev; 
+        prev = current;  
+        current = newTemp;
+    }
+    return prev;
+};
