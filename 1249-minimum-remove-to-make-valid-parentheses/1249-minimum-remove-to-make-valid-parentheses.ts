df558cc1fc7f80307e75e30410c73d527858fcff
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-28 22:08:57 +0800

    Time: 151 ms (46.67%), Space: 49 MB (44.44%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.ts
@@ -0,0 +1,29 @@
+const minRemoveToMakeValid = (s:string) => {
+  // Algorithmn O(n) - Time | O(n) Space
+  // loop through the string
+  //      store each left bracket index in a stack
+  //      if right bracket, pop the stack
+  // any left-bracket remaining in the stack will be replaced with ""
+  if (s.length === 0) return "";
+
+  let listofChars = s.split("");
+  let stack = [];
+
+  for (let index = 0; index < listofChars.length; index++) {
+    if (listofChars[index] === "(") {
+      stack.push(index);
+    } else if (listofChars[index] === ")" && stack.length) {
+      stack.pop();
+    } else if (listofChars[index] === ")") {
+      listofChars[index] = "";
+    }
+  }
+
+  while (stack.length > 0) {
+    // Assign -1 if pop value is undefined 
+    let value = stack.pop() ?? -1;
+    listofChars[value] = "";
+  }
+
+  return listofChars.join("");
+};
