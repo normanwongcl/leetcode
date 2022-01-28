a14860d10ed93a98706a3bf109ec1608d8c5ce10
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-01-28 16:43:29 +0800

    Time: 92 ms (54.02%), Space: 42.8 MB (10.60%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.ts
@@ -0,0 +1,30 @@
+interface IHash {
+  [key: string]: string | undefined;
+}
+
+// Hashmap to lookup left bracket
+const lookUpLeftBracket: IHash = {
+  "(": ")",
+  "{": "}",
+  "[": "]",
+};
+
+// O(n) - Time | O(n) - Space
+const isValid = (s: string, lookUp: IHash = lookUpLeftBracket) => {
+  if (s.length === 0) return true;
+
+  const stack: string[] = [];
+
+  for (let index = 0; index < s.length; index++) {
+    if (lookUp[s[index]]) {
+      stack.push(s[index]);
+    } else {
+      let leftBracket = stack.pop() ?? "";
+      if (s[index] !== lookUp[leftBracket]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
