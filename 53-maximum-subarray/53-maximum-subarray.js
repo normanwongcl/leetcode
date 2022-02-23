8fba1b877f34d06e63c21272a953577013666af1
Norman Wong <34085667+normanwongcl@users.noreply.github.com>
2022-02-23 11:13:18 +0800

    Time: 84 ms (92.47%), Space: 50.8 MB (15.00%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,16 @@
+/** Using sliding window pattern - O(n) - Time | O(1) Space
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = (nums) => {
+  let maxSum = nums[0], currentSum = nums[0];
+
+  for (let index = 1; index < nums.length; index++) {
+      currentSum = Math.max(nums[index], currentSum + nums[index]);
+      
+      if (currentSum > maxSum) {
+          maxSum = currentSum;
+      }
+  }
+  return maxSum;
+};
